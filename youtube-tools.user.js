8f5a780b5d52e171f0b09ae1c240e13efcd35e5c
Akari <akari310@users.noreply.github.com>
2026-05-01 20:59:45 +0700

    Update v0.0.0.8

--- youtube-tools.user.js
+++ youtube-tools.user.js
@@ -4,7 +4,7 @@
 // @description  Download high-quality video/audio, return dislikes, and more VIP features for YouTube and YouTube Music.
 // @description:vi Tải video/audio chất lượng cao, hiện nút dislike, và nhiều tính năng VIP khác cho YouTube và YouTube Music.
 // @homepage     https://github.com/akari310/
-// @version      0.0.0.7
+// @version      0.0.0.8
 // @author       Akari
 // @match        *://www.youtube.com/*
 // @match        *://music.youtube.com/*
@@ -4542,7 +4542,7 @@
     <div class="developer-mdcm">
       Developed by <a href="https://github.com/akari310" target="_blank"> <i class="fa-brands fa-github"></i> Akari</a>
     </div>
-    <span style="color: #fff" ;>v0.0.0.7</span>
+    <span style="color: #fff" ;>v0.0.0.8</span>
   </div>
   `;
   panel.innerHTML = safeHTML(menuHTML);
@@ -7541,7 +7541,7 @@ const CODE_STYLE = 'font-size: 14px; font-family: monospace;';
 
 console.log(
   '%cYoutube Tools Extension NEW UI\n' +
-  '%cRun %c(v0.0.0.7)\n' +
+  '%cRun %c(v0.0.0.8)\n' +
   'By: Akari.',
   HEADER_STYLE,
   CODE_STYLE,
